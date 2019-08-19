7a81887608fcd9294c92002758a5059754d2d3ec
Saksham Katiyar <saksham.katiyar@mayadata.io>
2019-08-19 16:19:42 +0530

    add defer

---
+++ src/defer/defer_func.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func ReadFile(filename string) ([]byte, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	return ioutil.ReadAll(f)
+}
+
+func main() {
+	f, _ := ReadFile("test.txt")
+	fmt.Println("%s", f)
+	fmt.Printf("%s\n", f)
+	fmt.Println(string(f))
+}
