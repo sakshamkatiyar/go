e345f31dbf575c30a32f730ea0f8c43cf7b73cfb
Saksham Katiyar <saksham.katiyar@mayadata.io>
2019-08-21 15:51:23 +0530

    add pipeline

--- src/concurrency/channel.go
+++ src/concurrency/channel.go
@@ -8,13 +8,15 @@ func main() {
 	// counter := make(chan int, 2) // no effect
 	// Creates a buffered channel with capacity of 3
 	nums := make(chan int, 3)
+
 	// counter <- 2 // fatal error -> deadlock
+	
 	go func() {
 		// Send value to the unbuffered channel
 		counter <- 1
-		counter <- 2
+		// counter <- 2
 		close(counter) // Closes the channel
-		// counter <- 5 // no effect
+		// counter <- 5 // no effect if not received
 	}()
 
 	go func() {
@@ -25,6 +27,7 @@ func main() {
 		nums <- 40 // panic
 		// nums <- 50 // deadlock
 	}()
+
 	// Read the value from unbuffered channel
 	fmt.Println(<-counter)
 	val, ok := <-counter // Trying to read from closed channel // blocking operation
@@ -32,6 +35,8 @@ func main() {
 		fmt.Println(val) // This won't execute
 		// fmt.Println("val", val)
 	}
+	// fmt.Println(<-counter)
+
 	// Read the 3 buffered values from the buffered channel
 	fmt.Println(<-nums)
 	fmt.Println(<-nums)
